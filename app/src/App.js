da563db2bb5697ff029c72fd171194dafc48df50
allenalvin333 <allenalvin333@gmail.com>
2021-12-13 14:00:08 +0530

    programs

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,36 @@
 import React from "react";
 import "./App.css";
+import {useState, useEffect} from 'react';
 import video from "./videos/sculpture.mp4";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async() => {
+      const result = await fetch('https://orangevalleycaa.org/api/videos').then(
+      response => response.json()
+      );
+      setData(result);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header>
         <h1>Videos App</h1>
       </header>
-      <h2>0. Offline Video</h2>
-      <div>
-        <video src={video} controls height={200} />
+
+      <div key='0'>
+        <h2>Offline Video</h2>
+        <video height={200} controls src={video} />
       </div>
+      {data.map(video => (
+      <div key={video.id}>
+          <h2>{video.name}</h2>
+          <video height={200} controls src={video.video_url} />
+      </div>
+      ))}
     </div>
   );
 }
